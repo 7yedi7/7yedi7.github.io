de92f46c6247db5a2fdaa3c89b441a6381e6be91
Student77 <155666959+Yedisowda@users.noreply.github.com>
2024-07-14 13:22:57 +0500

    14.07

--- js/qr_code_creator.js
+++ js/qr_code_creator.js
@@ -1,7 +1,20 @@
+let value_size;
+let qr_size
+window.onload = function(){
+    qr_size =  document.getElementById("qr_size")
+
+    qr_size.oninput =function(){
+        console.log(qr_size.value)
+        value_size = document.getElementById("value_size")
+        value_size.textContent = qr_size.value
+    }
+}
+
+
 function qr_generate(){
     console.log("inside qr")
     let qr_img = document.getElementById("qr_code_id");
     let qr_text = document.getElementById("qr_code_input").value
-    let x = 250;
+    let x = qr_size.value;
     qr_img.src = `https://api.qrserver.com/v1/create-qr-code/?size=${x}x${x}&data=${qr_text}`
 }
